9a4c41d852458bee69a8cbd0f85d24c1a36af183
kogicko240 <dmn120112@gmail.com>
2026-04-22 17:19:40 +0300

    Task03 Done, 17:20

--- Task03/logic.cpp
+++ Task03/logic.cpp
@@ -5,3 +5,27 @@
 // Необходимо разработать функцию (или программу), которая находит
 // количество строк, в которых положительных элементов больше 
 // чем всех остальных (отрицательных и нулевых).
+
+#include "logic.h"
+
+int count_rows_with_more_positive_values(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n, int m) {
+	int count_rows = 0;
+
+	for (int i = 0; i < n; i++) {
+		int positive_count = 0;
+		int non_positive_count = 0;
+
+		for (int j = 0; j < m; j++) {
+			if (matrix[i][j] > 0) {
+				positive_count++;
+			}
+			else {
+				non_positive_count++;
+			}
+		}
+		if (positive_count > non_positive_count) {
+			count_rows++;
+		}
+	}
+	return count_rows;
+}
