455b6ad47e65317190810f7ee1e57d652621ce6e
kogicko240 <dmn120112@gmail.com>
2026-04-22 17:13:11 +0300

    Task02 Done 17:13

--- Task02/logic.cpp
+++ Task02/logic.cpp
@@ -5,3 +5,18 @@
 // Необходимо разработать функцию (или программу), 
 // которая вычисляет сумму элементов матрицы, 
 // расположенных на главной и побочной диагоналях.
+
+#include "logic.h"
+
+int sum_main_and_second_diagonals_elements(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n) {
+	int sum = 0;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i == j || i + j == n - 1) {
+				sum += matrix[i][j];
+			}
+		}
+	}
+	return sum;
+}
